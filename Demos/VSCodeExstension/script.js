358b66d23553307be8a3c41e100904b043d39c4d
beckham jackson <bjack-s206@chusd.org>
2025-11-10 11:12:55 -0800

    Inital commit

---
+++ Demos/VSCodeExstension/script.js
@@ -0,0 +1,7 @@
+// This is a javascript comment
+//? This is a javascript comment
+//! This is a javascript comment
+//TODO This is a javascript comment
+//* This is a javascript comment
+
+window.alert("hello");
